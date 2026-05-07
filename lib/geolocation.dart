ef2fc00eb0b557ae50c8202989178c1f13d2b17c
Fizzrss <fizars123@gmail.com>
2026-05-07 07:29:41 +0700

    praktikum 7 langkah 1

--- lib/geolocation.dart
+++ lib/geolocation.dart
@@ -36,11 +36,9 @@ class _LocationScreenState extends State<LocationScreen> {
   }
 
   Future<Position> getPosition() async {
-    await Geolocator.requestPermission();
     await Geolocator.isLocationServiceEnabled();
     await Future.delayed(const Duration(seconds: 3));
-    Position? position =
-        await Geolocator.getCurrentPosition();
+    Position position = await Geolocator.getCurrentPosition();
     return position;
   }
 }
